241aa2aa0cd69d2f8a2898ac7004d5ee0921e311
Guilherme Cândido <guipretto5@gmail.com>
2021-12-20 16:26:11 -0300

    fix: remove cors from server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import resolvers from './resolvers.js';
 import typeDefs from './schema/index.js';
 
 const server = new ApolloServer({
-  typeDefs, resolvers
+  typeDefs, resolvers, cors: false
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
