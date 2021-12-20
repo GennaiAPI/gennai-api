cca05ceb85707eab373c91322a44c25b83b83f10
Guilherme Cândido <guipretto5@gmail.com>
2021-12-20 17:01:19 -0300

    feat: change from apollo-server to apollo-server-express

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import { StarWarsAPI } from './apis/starWarsApi.js'
+import express from 'express';
+import http from 'http';
 import resolvers from './resolvers.js';
 import typeDefs from './schema/index.js';
 
-const server = new ApolloServer({
-  typeDefs, resolvers, cors: false
-});
+const startServer = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+
+  const server = new ApolloServer({
+    typeDefs, resolvers, plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  server.applyMiddleware({
+    app,
+
+    // By default, apollo-server hosts its GraphQL endpoint at the
+    // server root. However, *other* Apollo Server packages host it at
+    // /graphql. Optionally provide this to match apollo-server.
+    path: '/graphql'
+  });
+
+  await new Promise(resolve => httpServer.listen({ port: process.env.PORT || 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:${process.env.PORT || 4000}${server.graphqlPath}`);
+  // server.listen({ port:  }).then(({ url }) => {
+  //   console.log(`🚀  Server ready at ${url}`);
+  // })
+}
+
+startServer()
