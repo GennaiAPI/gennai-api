15c60d48486ba194929efc5142d89c7bd8fe8b0a
Guilherme Cândido <guipretto5@gmail.com>
2021-12-20 15:43:33 -0300

    feat: update server port

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const server = new ApolloServer({
   typeDefs, resolvers
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
